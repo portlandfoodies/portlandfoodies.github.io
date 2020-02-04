b629d5315b659fb78b162029fcb34ac8d7ec705a
Qiacheng Li <qiacheng@babbage.cs.pdx.edu>
2020-02-04 15:41:18 -0800

    added category count

---
+++ scripts/category_count.py
@@ -0,0 +1,20 @@
+import json
+import csv
+import operator
+
+def main():
+  with open('../data/portland.json') as portland_json:
+    data = json.load(portland_json)
+    categories = dict()
+    for bs in data['businesses']:
+      for cg in bs['categories']:
+        categ = cg['title']
+        if cg['title'] in categories:
+          categories[categ] += 1
+        else:
+          categories[categ] = 1
+  with open('word_count.csv', 'w+') as ofile:
+    for key in categories.keys():
+      ofile.write("%s,%s\n"% (key, categories[key]))
+  
+main()
