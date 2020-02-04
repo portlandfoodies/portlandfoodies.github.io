f2dece45f69fe411914b95bbd49d7e388864a177
Qiacheng Li <qiacheng@babbage.cs.pdx.edu>
2020-02-04 15:22:22 -0800

    update

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,3 @@
-## this code is used for requesting restaurant infomation in Portland Oregon97266 using the Yelp Fusion API.
-## Yelp Fusion has a limit of up to 50 results returned back in a single request and up to 1000 total with the use of offset. 
-## 02/21/2020
-## PSU CS410 Data Analysis - Team Portlandfoodies
 import requests
 import json
 
@@ -9,19 +5,20 @@ import json
 
 
 def main():
-  api_key = "Bearer " + ""
+  api_key = "Bearer XdBia7yU2QMsgurA7Tyzm_A9lxiTDOqp-MW7lQYi0-6AMD7zc4odwN6FtDB-LvD0bAxLQCS6OPboZ38sEQ2LtpfRaVjaTHeIqZtt87sXe-Oxfr865KbbTT9ocn8nXnYx"
   headers = {'Authorization' : api_key}
   url='https://api.yelp.com/v3/businesses/search'
-  params = {'catagory':'food', 'location':'portland,or,97266', 'limit': '50', 'offset': '0', 'radius':40000}
+  params = {'catagory':'food', 'location':'portland,or97266', 'limit': '50', 'offset': '0', 'radius':40000}
   req = requests.get(url, params = params, headers = headers)
   data = json.loads(req.text)
 
   for offs in range(1, 20):
-    params = {'catagory':'food', 'location':'portland,or,97266', 'radius': 40000, 'limit': '50', 'offset': (offs*50)}
+    params = {'catagory':'food', 'location':'portland,or97266', 'radius': 40000, 'limit': '50', 'offset': (offs*50)}
     req = requests.get(url, params = params, headers = headers)
     temp = data['businesses']
     temp2 = json.loads(req.text)
-    temp.append(temp2['businesses'])
+    for bs in temp2["businesses"]:
+      temp.append(bs)
 
   with open('portland97266.json', 'w+', encoding='utf-8') as ofile:
      json.dump(data, ofile, ensure_ascii=False, indent=4)
---
+++ scripts/word_count.py
@@ -0,0 +1,32 @@
+import json
+import csv
+import operator
+
+def main():
+    with open('reviews.json') as reviews_json:
+      data = json.load(reviews_json)
+      total_str = ""
+      for bs in data['business_reviews']:
+        total_str += " " + bs['text']
+    odata = word_count(total_str)
+    #with open('wordcount.json', 'w+', encoding='utf-8') as ofile:
+      #json.dump(odata, ofile, ensure_ascii=False, indent=4)
+    
+    with open('word_count.csv', 'w') as ofile:
+      for key in odata.keys():
+        ofile.write("%s,%s\n"% (key, odata[key]))
+    
+   
+
+def word_count(str):
+    sentence = dict()
+    words = str.split()
+
+    for word in words:
+      if word in sentence:
+        sentence[word] += 1
+      else:
+        sentence[word] = 1
+    return sentence
+
+main()
